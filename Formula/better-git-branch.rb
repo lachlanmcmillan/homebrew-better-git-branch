27c70bb0b488b9ff4db016145e3b322dbfb65f7f
Lachlan McMillan <lachlanmcmillan@users.noreply.github.com>
2020-05-19 08:16:34 +1000

    Release v1.0.1
    rename the binary

--- Formula/better-git-branch.rb
+++ Formula/better-git-branch.rb
@@ -4,12 +4,12 @@
 class BetterGitBranch < Formula
   desc "Like git branch, but interactive"
   homepage "https://github.com/lachlanmcmillan/better-git-branch"
-  url "https://github.com/lachlanmcmillan/better-git-branch/releases/download/v1.0.0/better-git-branch-v1.0.0_MacOS.tar.gz"
-  sha256 "ccd3560cc4656828bd43cbf929b87f70f4c5c0fa255be57b579f00ab9afcb8f8"
-  version "1.0.0"
+  url "https://github.com/lachlanmcmillan/better-git-branch/releases/download/v1.0.1/better-git-branch-v1.0.1.MacOS.tar.gz"
+  sha256 "e58c2f3dca22e60e6c8577c22a9f1652d3c35be77b1cfa7f3ae105f9eda5a84c"
+  version "1.0.1"
 
   def install
-    bin.install "gbr"
+    bin.install "better-git-branch"
   end
 end
 
