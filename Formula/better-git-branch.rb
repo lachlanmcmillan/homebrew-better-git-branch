55b39b5ca887d96248e90e747e50198d20575361
Lachlan McMillan <lachlanmcmillan@users.noreply.github.com>
2020-05-20 11:12:47 +1000

    fix sha256

--- Formula/better-git-branch.rb
+++ Formula/better-git-branch.rb
@@ -5,7 +5,7 @@ class BetterGitBranch < Formula
   desc "Like git branch, but interactive"
   homepage "https://github.com/lachlanmcmillan/better-git-branch"
   url "https://github.com/lachlanmcmillan/better-git-branch/releases/download/v1.1.0/better-git-branch-v1.1.0.MacOS.tar.gz"
-  sha256 "98bef7c17d628add0fe4cafc632fab72f14adf35028f662fd4505c6be6f5c84d  better-git-branch-v1.1.0.tar.gz"
+  sha256 "98bef7c17d628add0fe4cafc632fab72f14adf35028f662fd4505c6be6f5c84d"
   version "1.1.0"
 
   def install
