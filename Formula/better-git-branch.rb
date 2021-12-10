68c506b878ce582ec404933fe85113a18c0dbb2b
Lachlan McMillan <lachlanmcmillan@users.noreply.github.com>
2021-12-10 16:49:24 +1100

    Release v1.3.0

--- Formula/better-git-branch.rb
+++ Formula/better-git-branch.rb
@@ -2,14 +2,14 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class BetterGitBranch < Formula
-  desc "Like git branch, but interactive"
+  desc "git branch, but interactive"
   homepage "https://github.com/lachlanmcmillan/better-git-branch"
-  url "https://github.com/lachlanmcmillan/better-git-branch/releases/download/v1.2.2/better-git-branch-v1.2.2.MacOS.tar.gz"
-  sha256 "a6452fe6c49ef598db797c8cfe0d22988efd5f3d8c84fc76656d7a53dbe7295a"
-  version "1.2.2"
+  url "https://github.com/lachlanmcmillan/better-git-branch/releases/download/v1.3.0/better-git-branch-v1.3.0.tar.gz"
+  sha256 "baa98cf24aa2587e29db2e68d41a9096616378444b5e5a6479f1274dfc20c689"
+  version "1.3.0"
 
   def install
-    bin.install "better-git-branch"
+    bin.install "gb"
   end
 end
 
